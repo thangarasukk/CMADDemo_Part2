cbe48ef0563b428cfa8827deea519a7fcfc6949d
Thangarasu Kumarasamy (tkumaras) <tkumaras@cisco.com>
2016-05-01 08:48:02 +0530

    infra package is created

---
+++ src/main/java/com/mydomain/infra/ServicesFactory.java
@@ -0,0 +1,34 @@
+package com.mydomain.infra;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+ 
+public class ServicesFactory {
+	
+	
+	private static ThreadLocal<Datastore> mongoTL = new ThreadLocal<Datastore>();
+	
+	/**
+	 * Method to retrieve a mongo database client from the thread local storage
+	 * @return
+	 */
+	public static Datastore getMongoDB(){
+		if(mongoTL.get()==null){
+			//MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+			MongoClientURI connectionString = new MongoClientURI("mongodb://http://173.36.55.45:27017");
+			MongoClient mongoClient = new MongoClient(connectionString);	
+			Morphia morphia = new Morphia();
+			morphia.mapPackage("com.mydomain.model");
+			Datastore datastore = morphia.createDatastore(mongoClient, "test");
+			datastore.ensureIndexes();
+			mongoTL.set(datastore);
+			return datastore;
+		}
+		return mongoTL.get();
+	}
+	
+}

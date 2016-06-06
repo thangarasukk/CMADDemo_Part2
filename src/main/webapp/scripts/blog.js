b6124f5922a2bc737ea119ef0584df5218e1a9d2
Thangarasu Kumarasamy (tkumaras) <tkumaras@cisco.com>
2016-06-06 18:42:52 +0530

    listing page, single post detail page, app blog page are added

--- src/main/webapp/scripts/blog.js
+++ src/main/webapp/scripts/blog.js
@@ -1,12 +1,24 @@
 (function(){
-	var app = angular.module('Airlines',['ngRoute']);
+	var app = angular.module('Airlines',['ngRoute','angularUtils.directives.dirPagination']);
 	
 	app.config(['$routeProvider', 
 	    function($routeProvider){
 			$routeProvider.
-                when('/signup',{
+				when('/allblogs',{
+					templateUrl:'allblogs.html',
+					controller:'AllBlogsController'
+				}).
+				when('/singleblog',{
+					templateUrl:'singleblog.html',
+					controller:'SingleBlogController'
+				}).
+				when('/singleblogpost',{
+					templateUrl:'singleblogpost.html',
+					controller:'SingleBlogPostController'
+				}).
+				when('/signup',{
 					templateUrl:'signup.html',
-					controller:'signupController'
+					controller:'SignupController'
 				}).
 				when('/users',{
 					templateUrl:'users.html',
@@ -91,6 +103,137 @@
 			  $scope.error = status;
 		  });
 	});
-	
+
+    app.controller('SignupController',function($http, $log, $scope){
+		var controller = this;
+		$scope.blogs=[];
+		$log.debug("Getting blogs...");
+
+		$http.get('rest/blog').
+		  success(function(data, status, headers, config) {
+			  $scope.blogs = data;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.error = status;
+		  });
+	});
+    
+	app.controller('AllBlogsController',function($http, $log, $scope, GlobalStroage){
+		var controller = this;
+		$scope.blogs=[];
+		$log.debug("Getting blogs...");
+        this.selectedBlog;
+
+		$http.get('rest/blog').
+		  success(function(data, status, headers, config) {
+			  $scope.blogs = data;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.error = status;
+		  });
+
+        $scope.updateSelectedBlog = function(blog){
+    		this.selectedBlog = blog;
+    		$log.debug("inside updateSelectedBlog");
+            $log.debug(blog);
+            GlobalStroage.setSelectedBlogId(blog.id);
+            $log.debug(GlobalStroage.getSelectedBlogId());
+    	}
+        
+        $scope.convertJSONDateToJavascriptDate = function(jsonDate){
+        	var dateStr = JSON.parse(jsonDate);
+        	return new Date(dateStr).toUTCString();
+        }
+	});
+
+	app.controller('SingleBlogController',function($http, $log, $scope, GlobalStroage){
+		var controller = this;
+        var selectedBlogId = GlobalStroage.getSelectedBlogId();
+		$scope.blog=[];
+		$scope.loading = true;
+		$log.debug("Getting blog in SingleBlogController...");
+		$http.get('rest/blog/'+selectedBlogId).
+		  success(function(data, status, headers, config) {
+			  $scope.blog = data;
+			  $scope.loading = false;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.loading = false;
+			  $scope.error = status;
+		  });
+
+        $scope.convertJSONDateToJavascriptDate = function(jsonDate){
+        	var dateStr = JSON.parse(jsonDate);
+        	return new Date(dateStr).toUTCString();
+        }
+	});
+
+    app.service('GlobalStroage', function($log) {
+        this.selectedBlogId = "defaultblogID";
+        
+        this.getSelectedBlogId = function(){
+                $log.debug("Inside getSelectedBlogId..");
+                return this.selectedBlogId;
+            }        
+        this.setSelectedBlogId = function(selectedBlogId){
+                $log.debug("Inside setSelectedBlogId..");
+                this.selectedBlogId = selectedBlogId;
+            }   
+    });
+
+    app.service('Utility', function($log) {
+        this.convertJSONDateToJavascriptDate = function(jsonDate){
+        	var dateStr = JSON.parse(jsonDate);
+        	return new Date(dateStr).toUTCString();
+        }
+ 
+    });
+
+	app.controller('SingleBlogPostController',function($http, $log, $scope, GlobalStroage){
+		var controller = this;
+		$scope.blog=[];
+		$scope.loading = true;
+		$log.debug("SingleBlogPostController...");
+        $scope.showEditForm=false;
+        $scope.showAddForm=true;
+        $scope.blog.title="";
+        $scope.blog.content="";
+        $scope.blog.tags="";
+        $scope.blog.postedUserName="test_name";
+        $scope.blog.postedUserId="5722ffa441fbd6d042b07202";
+
+        $log.debug(GlobalStroage.getSelectedBlogId());
+
+ 		$scope.addBlog = function(blog){
+     		$log.debug("SingleBlogPostController...in addBlog");
+ 		    $log.debug(blog);
+
+            var data = JSON.stringify(
+                    {
+                      title: blog.title, 
+                      content: blog.content,
+                      tags: blog.tags,
+                      postedUserName : blog.postedUserName,
+                      postedUserId : blog.postedUserId
+                    });
+                 /* alert( data); */
+
+        $log.debug(data);
+        $log.debug("rest/blog");
+        $http.post("rest/blog",data)
+			.success(function(data){
+				$log.debug(data);
+                $scope.showEditForm=true;
+                $scope.showAddForm=false;
+			})
+            .error(function(data, status, headers, config) {
+                  $log.debug("error occured");
+				  $scope.error = status;
+				  $scope.showEditForm=false;
+			});
+            
+		};
+	});
+    
 })();
 

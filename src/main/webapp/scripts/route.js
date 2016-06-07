cca5f1c589be20f4998aba175fba92049793ff46
Animesh Kumar Bhadra <animesh.bhadra@gmail.com>
2016-06-07 18:34:52 +0530

    clean up

--- src/main/webapp/scripts/route.js
+++ src/main/webapp/scripts/route.js
@@ -4,14 +4,24 @@ angular.module('CMAD').config(function($routeProvider){
 		controller : 'LoginController',
 		controllerAs : 'loginCtrl'
 	})
-
 	.when('/signup', {
 		templateUrl : 'tmpl/signup.html',
 		controller : 'SignUpController',
 		controllerAs : 'signupCtrl'
-	})
-
-	.when('/', {
+	}).
+	when('/allblogs',{
+		templateUrl:'tmpl/allblogs.html',
+		controller:'AllBlogsController'
+	}).
+	when('/singleblog',{
+		templateUrl:'tmpl/singleblog.html',
+		controller:'SingleBlogController'
+	}).
+	when('/singleblogpost',{
+		templateUrl:'tmpl/singleblogpost.html',
+		controller:'SingleBlogPostController'
+	}).
+	when('/', {
 		templateUrl : 'tmpl/home.html',
 		controller : 'HomeController',
 		controllerAs : 'homeCtrl'
